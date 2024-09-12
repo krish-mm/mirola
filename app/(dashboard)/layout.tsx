96305c448421edbb061b21448ea5e816012d2f22
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-13 01:06:26 +0530

    Dashboard SideBar Navbar orgBoard rendered

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "./_components/navbar"
+import { OrgSidebar } from "./_components/org-sidebar"
 import { Sidebar } from "./_components/sidebar/page"
 
 interface DashboardLayoutProps {
@@ -12,7 +14,13 @@ const DashboardLayout = ({
         <main className="h-full">
             <Sidebar />
             <div className="pl-[60px] h-full">
-                {children}
+                <div className="flex gap-x-3 h-full">
+                    <OrgSidebar/>
+                    <div className="h-full flex-1">
+                        <Navbar/>
+                        {children}
+                    </div>
+                </div>
             </div>
         </main>
     )
