9edfefc200cacc1279ee60c65f7e7b95bb5279d2
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-13 02:27:58 +0530

    Hint tooltip for sidebar added

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -5,15 +5,23 @@ import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import Hint from "@/components/hint";
 
 const NewButton = () => {
     return (
         <Dialog>
             <DialogTrigger asChild>
                 <div className='aspect-square'>
-                    <button className='bg-white/50 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition'>
-                        <Plus className='text-[#1C1C1E]' />
-                    </button>
+                    <Hint
+                        label='Create organization'
+                        side='right'
+                        align='start'
+                        sideOffset={18}
+                    >
+                        <button className='bg-white/50 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition'>
+                            <Plus className='text-[#1C1C1E]' />
+                        </button>
+                    </Hint>
                 </div>
             </DialogTrigger>
             <DialogContent className='p-0 bg-transparent border-none max-w-[480px]'>
