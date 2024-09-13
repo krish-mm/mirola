a4643807c37c071b8b64de03dc6da4b62de18819
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-13 16:09:06 +0530

    Search modified and invite button added

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
-import { SearchInput } from "./search-input"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+import { InviteButton } from "./invite-button"
+import SearchInput from "./search-input"
 
 
 
@@ -11,6 +12,31 @@ export const Navbar = () => {
             <div className="hidden lg:flex-1 lg:flex">
                 <SearchInput/>
             </div>
+            <div className="block lg:hidden flex-1">
+            <OrganizationSwitcher
+                hidePersonal
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            maxWidth: "376px"
+                        },
+                        organizationSwitcherTrigger: {
+                            padding: "6px",
+                            width: "100%",
+                            borderRadius: "8px",
+                            border: "1px solid #E5E7EB",
+                            justifyContent: "space-between",
+                            backgroundColor: "white"
+                        }
+                    }
+                }}
+            />
+            </div>
+            <InviteButton/>
             <UserButton/>
         </div>
     )
--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,44 +1,46 @@
-"use client"
+"use client";
 
-import qs from "query-string"
-import { Search } from "lucide-react"
-import { useDebounce } from "usehooks-ts"
-import { useRouter } from "next/navigation"
-import { ChangeEvent, useEffect, useState } from "react"
-import { Input } from "@/components/ui/input"
+import React, { ChangeEvent, useEffect, useState } from "react";
+import qs from "query-string";
+import { useDebounce } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
-export const SearchInput = () => {
-
+const SearchInput = () => {
     const router = useRouter();
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState("");
     const debouncedValue = useDebounce(value, 500);
 
-    const handleChnage = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value)
-    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
 
     useEffect(() => {
-        const url = qs.stringifyUrl({
-            url: "/",
-            query: {
-                search: debouncedValue,
+        const url = qs.stringifyUrl(
+            {
+                url: "/",
+                query: {
+                    search: debouncedValue,
+                },
             },
-        }, { skipEmptyString: true, skipNull: true });
+            { skipEmptyString: true, skipNull: true }
+        );
 
-        router.push(url)
-    }, [debouncedValue, router])
+        router.push(url);
+    }, [debouncedValue, router]);
 
     return (
-        <div className="w-full relative">
-            <Search
-                className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4 "
-            />
+        <div className='w-full relative'>
+            <Search className='absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4' />
             <Input
-                className="w-full max-w-[516px] pl-9 "
-                placeholder="Search Miro"
-                onChange={handleChnage}
+                className='w-full max-w-[516px] pl-9 transition'
+                placeholder='Search boards'
+                onChange={handleChange}
                 value={value}
             />
         </div>
-    )
-}
+    );
+};
+
+export default SearchInput;
