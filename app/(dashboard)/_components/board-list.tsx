46050c65e1f60f5fea4cfd56584caf71b1b01309
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-14 01:06:55 +0530

    Board Card added :e

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -3,7 +3,8 @@
 import EmptyBoards from "./empty-board";
 import EmptyFavorites from "./empty-favorite";
 import EmptySearch from "./empty-search";
-
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 interface BoardListProps {
     orgId:string,
     query:{
@@ -14,7 +15,15 @@ interface BoardListProps {
 
 export const BoardList = ({orgId,query}:BoardListProps) => {
 
-    const data = [];
+    const data = useQuery(api.boards.get,{orgId})
+
+    if(data === undefined){
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
 
     if(!data?.length && query.search){
         return(
@@ -42,7 +51,24 @@ export const BoardList = ({orgId,query}:BoardListProps) => {
 
     return (
         <div>
-            {JSON.stringify(query)}
+            <h2 className="text-3xl">
+                {query.favorites ? "Favorites Board" : "Team Boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                {data.map((board) => (
+                    <BoardCard
+                        key={board._id}
+                        id={board._id}
+                        title={board.title}
+                        imageUrl={board.imageUrl}
+                        authorId={board.authorId}
+                        authorName={board.authorName}
+                        createdAt={board._creationTime}
+                        orgId={board.orgId}
+                        isFavorite={false}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
