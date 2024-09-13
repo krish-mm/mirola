1810ccf508bedd61cb2ce41b93aa78f8fd2536e3
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-14 00:01:41 +0530

    Added Empty favorite and search

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import EmptyFavorites from "./empty-favorite";
+import EmptySearch from "./empty-search";
+
 interface BoardListProps {
     orgId:string,
     query:{
@@ -9,6 +12,33 @@ interface BoardListProps {
 }
 
 export const BoardList = ({orgId,query}:BoardListProps) => {
+
+    const data = [];
+
+    if(!data?.length && query.search){
+        return(
+            <div>
+                <EmptySearch/>
+            </div>
+        )
+    }
+
+    if(!data?.length && query.favorites){
+        return(
+            <div>
+                <EmptyFavorites/>
+            </div>
+        )
+    }
+
+    if(!data?.length){
+        return(
+            <div>
+                No miro
+            </div>
+        )
+    }
+
     return (
         <div>
             {JSON.stringify(query)}
