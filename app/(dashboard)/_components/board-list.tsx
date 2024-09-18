92ae5d9408c6b3852d7f7b0f49c058858a43b0fd
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-18 19:01:29 +0530

    Board route updated

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -17,7 +17,10 @@ interface BoardListProps {
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
 
-    const data = useQuery(api.boards.get, { orgId })
+    const data = useQuery(api.boards.get, {
+        orgId,
+        ...query
+    })
 
     if (data === undefined) {
         return (
@@ -26,8 +29,8 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
                     {query.favorites ? "Favorites Board" : "Team Boards"}
                 </h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-                    <NewBoardButton orgId={orgId} disabled/>
-                    <BoardCard.Skeleton/>
+                    <NewBoardButton orgId={orgId} disabled />
+                    <BoardCard.Skeleton />
                 </div>
             </div>
         )
