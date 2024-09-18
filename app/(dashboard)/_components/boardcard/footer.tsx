958cc58b85e5d51878585f1529308cbd7533c56a
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-18 18:12:07 +0530

    Fav and Unfav functionality added

--- app/(dashboard)/_components/boardcard/footer.tsx
+++ app/(dashboard)/_components/boardcard/footer.tsx
@@ -1,44 +1,52 @@
-import React from "react";
-import { Star } from "lucide-react";
+import { MouseEvent, MouseEventHandler } from 'react'
+import { Star } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
-import { cn } from "@/lib/utils";
-import { getDisplayName } from "next/dist/shared/lib/utils";
+interface FooterProps {
+    isFavorite: boolean
+    disabled: boolean
+    authorLabel: string
+    createdAtLabel: string
+    title: string
+    onClick: () => void
+}
 
 const Footer = ({
     isFavorite,
-    title,
     authorLabel,
     createdAtLabel,
-    onClick,
+    title,
     disabled,
-}:any) => {
-    const handleClick = (e:any) => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        onClick();
-    };
+    onClick,
+}: FooterProps) => {
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (
+        event: MouseEvent<HTMLButtonElement>,
+    ) => {
+        event.stopPropagation()
+        event.preventDefault()
+        onClick()
+    }
 
     return (
-        <div className='relative bg-white p-3'>
-            <p className='text-[13px] truncate max-w-[calc(100%-20px)]'>{title}</p>
-            <p className='opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate'>
+        <div className="relative bg-white p-3">
+            <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
+            <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
                 {authorLabel}, {createdAtLabel}
             </p>
             <button
                 disabled={disabled}
                 onClick={handleClick}
                 className={cn(
-                    "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
-                    disabled && "cursor-not-allowed opacity-75"
+                    'opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600',
+                    disabled && 'cursor-not-allowed opacity-75',
                 )}
             >
                 <Star
-                    className={cn("h-4 w-4", isFavorite && "fill-blue-600 text-blue-600")}
+                    className={cn('h-4 w-4', isFavorite && 'fill-blue-600 text-blue-600')}
                 />
             </button>
         </div>
-    );
-};
+    )
+}
 
-export default Footer;
+export default Footer
