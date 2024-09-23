4d358cf57f8f2df749e5a2ea8973402ffec237c8
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-24 04:07:30 +0530

    Revert "Multi cursors added"
    This reverts commit 4d06e5c27f7472ff98399c8a07d2f23f47cda232.

--- components/room.tsx
+++ components/room.tsx
@@ -5,20 +5,15 @@ import { RoomProvider } from "@/liveblocks.config"
 import { ClientSideSuspense } from "@liveblocks/react"
 
 interface RoomProps {
-    children: ReactNode,
-    roomId: string,
-    fallback: NonNullable<ReactNode> | null,
+    children : ReactNode,
+    roomId : string,
+    fallback:  NonNullable<ReactNode> | null,
 }
 
 
-export const Room = ({ children, roomId, fallback }: RoomProps) => {
+export const Room = ({ children,roomId,fallback } : RoomProps) => {
     return (
-        <RoomProvider
-            id={roomId}
-            initialPresence={{
-                cursor: null,
-            }}
-        >
+        <RoomProvider id={roomId} initialPresence={{}}>
             <ClientSideSuspense fallback={fallback}>
                 {() => children}
             </ClientSideSuspense>
