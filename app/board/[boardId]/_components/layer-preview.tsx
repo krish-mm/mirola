182c7abe15f1f42f9b8b978c373a8a9b5e899fd2
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-28 23:55:46 +0530

    Added layer preview

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -3,6 +3,9 @@ import { PointerEvent, memo } from "react";
 import {useStorage} from "@/liveblocks.config";
 import {LayerType} from "@/types/canvas";
 import Rectangle from "./rectangle";
+import Ellipse from "./ellipse";
+import Text from "./text";
+import Note from "./note";
 interface LayerPreviewProps {
     id: string;
     onLayerPointerDown: (e: PointerEvent, layerId: string) => void;
@@ -24,11 +27,26 @@ const LayerPreview = ({ id, onLayerPointerDown, selectionColor }:LayerPreviewPro
                 onPointerDown={onLayerPointerDown}
             />);
         case LayerType.Ellipse:
-            return <div>Ellipse</div>;
+            return (<Ellipse
+                id={id}
+                layer={layer}
+                selectionColor={selectionColor}
+                onPointerDown={onLayerPointerDown}
+            />);
         case LayerType.Text:
-            return <div>Text</div>;
+            return <Text
+                id={id}
+                layer={layer}
+                selectionColor={selectionColor}
+                onPointerDown={onLayerPointerDown}
+            />;
         case LayerType.Note:
-            return <div>Note</div>;
+            return <Note
+                id={id}
+                layer={layer}
+                selectionColor={selectionColor}
+                onPointerDown={onLayerPointerDown}
+            />;
         default:
             console.warn("Unknown layer type");
             return null;
